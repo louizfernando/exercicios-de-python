64b3ce7359daaf1f49ff46c7c048c89be7f886f9
louizfernando <louizsilva093@gmail.com>
2023-03-23 18:39:25 -0400

    exercicios python

--- 035.py
+++ 035.py
@@ -1,9 +1,9 @@
 from emoji import emojize
 print('Vamos ver ser você consegue formar um trangulo!')
-a = int(input('Primeiro valor: '))
-b = int(input('Segundo valor: '))
-c = int(input('Terceiro valor: '))
-if a + b > c:
+a = float(input('Primeiro valor: '))
+b = float(input('Segundo valor: '))
+c = float(input('Terceiro valor: '))
+if a < b + c or b < a + c or c < a + b:
     print(emojize(':smiling_face_with_sunglasses: PARABÉNS! VOCÊ CONSEGUIU FORMAR UM TRIÂNGULO. :smiling_face_with_sunglasses:'))
 else:
     print(emojize(':man_facepalming::crying_face: NÃO FOI DESSA VEZ :crying_face::man_facepalming:'))
