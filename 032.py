0063ee0690b7f4bf4f6d9f8d7a76969df63a7b89
louizfernando <louizsilva093@gmail.com>
2023-03-23 14:31:10 -0400

    exercicios python

---
+++ 032.py
@@ -0,0 +1,5 @@
+ano = int(input('Digite um ano qualquer: '))
+if ano % 4 == 0:
+    print('O ano de {} é um ano bissexto.'.format(ano))
+else:
+    print('O ano de {} não é um ano bissexto.'.format(ano))
